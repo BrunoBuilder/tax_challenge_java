32485b3828928f9eb914c2c3bf582208ad491f16
BrunoBuilder <brunofernandotadeu@hotmail.com>
2022-06-06 12:17:25 -0300

    Data input

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,19 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		double annualIncomeSalary, annualIncomeServiceProvision, annualIncomeCapitalGain, 
+		medicalExpenses, educationalExpenses;
 		
+		System.out.print("Annual income with salary: $ ");
+		annualIncomeSalary = sc.nextDouble();
+		System.out.print("Annual income with service provision: $ ");
+		annualIncomeServiceProvision = sc.nextDouble();
+		System.out.print("Annual income with capital gain: $ ");
+		annualIncomeCapitalGain = sc.nextDouble();
+		System.out.print("Medical expenses: $ ");
+		medicalExpenses = sc.nextDouble();
+		System.out.print("Educational expenses: $ ");
+		educationalExpenses = sc.nextDouble();
 		
 		sc.close();
 	}
